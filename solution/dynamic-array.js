ccf498f8726d8e0ea93729080f14eeec0313ac8e
dirtroaddev <rongoodwindev@gmail.com>
2024-02-22 17:35:27 -0500

    Finished the dynamic array

--- solution/dynamic-array.js
+++ solution/dynamic-array.js
@@ -19,7 +19,15 @@ class DynamicArray {
   unshift(val) {
 
     // Your code here
-    
+    if(this.length === this.capacity) {
+      this.capacity *= 2;
+    }
+    for(let i = this.data.length; i > 0; i -= 1) {
+      this.data[i] = this.data[i - 1]
+    }
+    this.data[0] = val;
+    this.length += 1;
+
   }
 
 }
